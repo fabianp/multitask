472e684011b47d31b6e7e0f6883aa2c86d83bc0f
Fabian Pedregosa <fabian@fseoane.net>
2013-09-19 17:59:00 +0200

    more

--- test_res.py
+++ test_res.py
@@ -1,4 +1,4 @@
-import sys
+
 import tempfile
 import numpy as np
 from datetime import datetime
@@ -10,6 +10,10 @@ from scipy import linalg, signal, sparse
 
 from scipy.sparse import linalg as splinalg
 from nipy.modalities.fmri import hemodynamic_models as hdm
+
+import multitask as mt
+import hrf_estimation as he
+
 
 DIR = tempfile.mkdtemp()
 fir_length = 20
@@ -28,36 +32,25 @@ for n_session, Y_session in enumerate(np.split(Y, 5)):
     X_test = X_session[5 * k:]
     Y_train = Y_session[:5 * k]
     Y_test = Y_session[5 * k:]
-    n_task = 100
-    import ipdb; ipdb.set_trace()
+    n_task = 1000
     print('Done')
-
 
     print('Detrending')
     from multitask.savitzky_golay import savgol_filter
-
-    if False:
-        Y_train = scipy.signal.detrend(
-            Y_train[:, :n_task], bp=np.linspace(0, X_train.shape[0], 5).astype(np.int),
-            axis=0, type='linear')
-        Y_test = scipy.signal.detrend(
-            Y_test[:, :n_task], bp=np.linspace(0, X_test.shape[0], 2).astype(np.int),
-            axis=0, type='linear')
-    else:
-        Y_train = Y_train.reshape((5, 672, -1))
-        Y_train = Y_train[..., :n_task] - savgol_filter(
-            Y_train[..., :n_task], 91, 4, axis=1)
-        Y_train = scipy.signal.detrend(Y_train, axis=1)
-        Y_test = Y_test.reshape((2, 672, -1))
-        Y_test = Y_test[..., :n_task] - savgol_filter(
-            Y_test[..., :n_task], 91, 4, axis=1)
-        Y_test = scipy.signal.detrend(Y_test, axis=1)
-        norm_train = np.sqrt((Y_train * Y_train).sum(1))
-        norm_test = np.sqrt((Y_test * Y_test).sum(1))
-        for i in range(5):
-            Y_train[i] /= norm_train[i]
-        for i in range(2):
-            Y_test[i] /= norm_test[i]
+    Y_train = Y_train.reshape((5, 672, -1))
+    Y_train = Y_train[..., :n_task] - savgol_filter(
+        Y_train[..., :n_task], 91, 4, axis=1)
+    Y_train = scipy.signal.detrend(Y_train, axis=1)
+    Y_test = Y_test.reshape((2, 672, -1))
+    Y_test = Y_test[..., :n_task] - savgol_filter(
+        Y_test[..., :n_task], 91, 4, axis=1)
+    Y_test = scipy.signal.detrend(Y_test, axis=1)
+    norm_train = np.sqrt((Y_train * Y_train).sum(1))
+    norm_test = np.sqrt((Y_test * Y_test).sum(1))
+    for i in range(5):
+        Y_train[i] /= norm_train[i]
+    for i in range(2):
+        Y_test[i] /= norm_test[i]
 
     Y_train = Y_train.reshape((3360, n_task))
     Y_test = Y_test.reshape((1344, n_task))
@@ -74,31 +67,8 @@ for n_session, Y_session in enumerate(np.split(Y, 5)):
         v0 = np.load('v0.npy')
 
 
-    import multitask as mt
-
     print('Calling rank_one')
-
-    def classic_to_obo(classic_design, fir_length=1):
-        """
-        Will convert a classic or fir design to the one by one setting.
-        Returns one matrix per event, containing event related regressors
-        on the left and sum of remaining regressors on the right.
-        """
-
-        event_regressors = classic_design.reshape(
-            len(classic_design), -1, fir_length)
-
-        regressor_sum = event_regressors.sum(axis=1)
-
-        remaining_regressors = (regressor_sum[:, np.newaxis, :] -
-                                event_regressors)
-        together = np.concatenate([event_regressors,
-                                   remaining_regressors], axis=2)
-
-        return together.transpose(1, 0, 2)
-
     u0 = np.repeat(canonical, n_task).reshape((-1, n_task))
-    going_obo = len(sys.argv) > 1 and sys.argv[1] == 'OBO'
 
     print('GOING OBO')
     start = datetime.now()
@@ -112,7 +82,6 @@ for n_session, Y_session in enumerate(np.split(Y, 5)):
     print('RANK ONE CLASSIC SETTING')
     start = datetime.now()
     u0 = canonical
-    import hrf_estimation as he
     out = he.rank_one(
         X_train, Y_train, fir_length, u0=u0, v0=v0,
         verbose=1)
@@ -163,7 +132,7 @@ for n_session, Y_session in enumerate(np.split(Y, 5)):
     pl.ylabel('MSE glm Canonical')
     pl.axis('tight')
     pl.savefig('mse_r1_vs_can_%03d.png' % n_session)
-    pl.show()
+#    pl.show()
 
     pl.figure()
     pl.title('GLM rank one OBO vs GLM canonical, session %s' % n_session)
@@ -176,7 +145,7 @@ for n_session, Y_session in enumerate(np.split(Y, 5)):
     pl.axis('equal')
     pl.axis('tight')
     pl.savefig('mse_obo_vs_can_%03d.png' % n_session)
-    pl.show()
+#    pl.show()
 
 
     pl.figure()
@@ -190,4 +159,4 @@ for n_session, Y_session in enumerate(np.split(Y, 5)):
     pl.axis('equal')
     pl.axis('tight')
     pl.savefig('mse_obo_vs_r1_%03d.png' % n_session)
-    pl.show()
+#    pl.show()
