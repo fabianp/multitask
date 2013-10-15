ad38a4d956b6252fbed28bac912c84321a185aee
Fabian Pedregosa <fabian@fseoane.net>
2013-10-15 12:48:24 +0200

    more

--- multitask/__init__.py
+++ multitask/__init__.py
@@ -5,3 +5,4 @@ from low_rank_ import low_rank, rank_one, khatri_rao, rank_one_gradproj, \
 from ridge_ import ridge
 from trace_ import trace, trace_frankwolfe
 from savitzky_golay import savgol_filter
+import optimize
